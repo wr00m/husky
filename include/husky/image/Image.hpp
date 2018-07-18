7a67bbe5cbf12356df64211450376e1e779d52ee
wr00m <gogs@fake.local>
2018-07-18 16:48:40 +0200

    Save image files (stb_image_write)

--- include/husky/image/Image.hpp
+++ include/husky/image/Image.hpp
@@ -14,6 +14,7 @@ public:
   Image(int width, int height, int bytesPerPixel);
   virtual ~Image();
 
+  bool save(const std::string &filePath) const;
   bool valid() const;
   const std::uint8_t* data() const;
 
--- src/husky/image/Image.cpp
+++ src/husky/image/Image.cpp
@@ -1,16 +1,20 @@
 #include <husky/image/Image.hpp>
 #include <husky/Log.hpp>
+#include <filesystem>
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #define STBI_MSC_SECURE_CRT
 #include "stb_image_write.h"
 
+namespace fs = std::experimental::filesystem;
+
 namespace husky {
 
 Image Image::load(const std::string &filePath)
 {
   stbi_set_flip_vertically_on_load(true); // Match texture coordinates
+
   int width, height, bytesPerPixel;
   if (stbi_uc *bytes = stbi_load(filePath.c_str(), &width, &height, &bytesPerPixel, STBI_default)) {
     return { width, height, bytesPerPixel, bytes };
@@ -43,6 +47,47 @@ Image::Image(int width, int height, int bytesPerPixel, std::uint8_t *stbBytes)
   else {
     bytes = static_cast<std::uint8_t*>(stbi__malloc(totalByteCount)); // Allocate byte array
   }
+}
+
+bool Image::save(const std::string &filePath) const
+{
+  // Note: This causes an exception for JPEG (594x594 RGB)
+  //stbi_flip_vertically_on_write(true); // Match texture coordinates
+
+  const auto f = fs::u8path(filePath);
+  const auto p = f.parent_path();
+  if (!fs::is_directory(p) && !fs::create_directories(p)) {
+    Log::warning("Failed to create directory: %s", p.string().c_str());
+    return false;
+  }
+
+  std::string ext = f.extension().string();
+  std::transform(ext.begin(), ext.end(), ext.begin(), ::tolower);
+
+  bool ok = true;
+
+  if (ext == ".png") {
+    ok = stbi_write_png(filePath.c_str(), width, height, bytesPerPixel, bytes, 0);
+  }
+  else if (ext == ".jpg" || ext == ".jpeg") {
+    ok = stbi_write_jpg(filePath.c_str(), width, height, bytesPerPixel, bytes, 90);
+  }
+  else if (ext == ".bmp") {
+    ok = stbi_write_bmp(filePath.c_str(), width, height, bytesPerPixel, bytes);
+  }
+  else if (ext == ".tga") {
+    ok = stbi_write_tga(filePath.c_str(), width, height, bytesPerPixel, bytes);
+  }
+  else {
+    Log::warning("Unsupported image file extension: %s", ext.c_str());
+  }
+
+  if (!ok) {
+    Log::warning("Failed to save image: %s", filePath.c_str());
+    return false;
+  }
+
+  return true;
 }
 
 bool Image::valid() const
